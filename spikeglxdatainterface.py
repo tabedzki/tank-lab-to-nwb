f98af4d4f0a3d8c84ac4d3c44bf43ec3b74a4c7b
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2020-10-20 09:49:20 -0400

    Delete spikeglxdatainterface.py

--- spikeglxdatainterface.py
+++
@@ -1,7 +0,0 @@
-"""Authors: Cody Baker and Ben Dichter."""
-from .baserecordingextractorinterface import BaseRecordingExtractorInterface
-from spikeextractors import SpikeGLXRecordingExtractor
-
-
-class SpikeGLXRecordingInterface(BaseRecordingExtractorInterface):
-    RX = SpikeGLXRecordingExtractor
