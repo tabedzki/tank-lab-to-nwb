b37077fdcba88a61bbcb17eefcbb68e3287143e6
Cody Baker <cbaker9@nd.edu>
2020-11-05 14:19:05 -0500

    simplified main conversion script; debugged PR #13; added alternative method for synchronizing with ttl pulses

--- tank_lab_to_nwb/convert_towers_task/towersnwbconverter.py
+++ tank_lab_to_nwb/convert_towers_task/towersnwbconverter.py
@@ -1,6 +1,7 @@
 """Authors: Cody Baker and Ben Dichter."""
 import os
 from datetime import datetime, timedelta
+from pathlib import Path
 
 from dateutil.parser import parse as dateparse
 from isodate import duration_isoformat
@@ -16,16 +17,9 @@ class TowersNWBConverter(NWBConverter):
         VirmenData=VirmenDataInterface,
     )
 
-    def __init__(self, **input_args):
-        self._recording_type = 'SpikeGLXRecording'
-        super().__init__(**input_args)
-
-    def get_recording_type(self):
-        return self._recording_type
-
     def get_metadata(self):
-        session_path = self.data_interface_objects['VirmenData'].input_args['folder_path']
-        subject_path, session_id = os.path.split(session_path)
+        file_path = Path(self.data_interface_objects['VirmenData'].input_args['file_path'])
+        session_id = file_path.stem
 
         session_name = os.path.splitext(session_id)[0]
         date_text = [name for name in session_name.split('_') if name.isdigit()][0]
@@ -40,42 +34,13 @@ class TowersNWBConverter(NWBConverter):
                 institution="Princeton",
                 lab="Tank"
             ),
-            Subject=dict(
-            ),
-            # self.get_recording_type(): {
-            #     'Ecephys': {
-            #         'subset_channels': all_shank_channels,
-            #         'Device': [{
-            #             'description': session_id + '.xml'
-            #         }],
-            #         'ElectrodeGroup': [{
-            #             'name': f'shank{n+1}',
-            #             'description': f'shank{n+1} electrodes'
-            #         } for n, _ in enumerate(shank_channels)],
-            #         'Electrodes': [
-            #             {
-            #                 'name': 'shank_electrode_number',
-            #                 'description': '0-indexed channel within a shank',
-            #                 'data': shank_electrode_number
-            #             },
-            #             {
-            #                 'name': 'group_name',
-            #                 'description': 'the name of the ElectrodeGroup this electrode is a part of',
-            #                 'data': shank_group_name
-            #             }
-            #         ],
-            #         'ElectricalSeries': {
-            #             'name': 'ElectricalSeries',
-            #             'description': 'raw acquisition traces'
-            #         }
-            #     }
-            # },
+            Subject=dict(),
             SpikeGLXRecording=None,
             VirmenData=dict()
         )
 
-        if os.path.isfile(session_path + ".mat"):
-            session_data = convert_mat_file_to_dict(mat_file_name=session_path)
+        if file_path.is_file():
+            session_data = convert_mat_file_to_dict(mat_file_name=file_path)
             subject_data = session_data['log']['animal']
 
             key_map = dict(name='subject_id', importWeight='weight')
@@ -84,6 +49,6 @@ class TowersNWBConverter(NWBConverter):
             age_in_iso_format = duration_isoformat(timedelta(weeks=subject_data['importAge']))
             metadata['Subject'].update(age=age_in_iso_format)
         else:
-            print(f"Warning: no subject file detected for session {session_path}!")
+            print(f"Warning: no subject file detected for session {session_id}!")
 
         return metadata
