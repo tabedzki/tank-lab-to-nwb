e55266d9519ccd4f7f0c4a99642d59aad1892be1
Cody Baker <cbaker9@nd.edu>
2020-12-18 19:23:05 -0500

    updating processing pipeline to remove support for writing to NWB from pickled objects; now works in append mode along all trajectories

--- tank_lab_to_nwb/convert_towers_task/convert_towers_processed.py
+++ tank_lab_to_nwb/convert_towers_task/convert_towers_processed.py
@@ -3,68 +3,58 @@ from pathlib import Path
 from isodate import duration_isoformat
 from datetime import timedelta
 
-import nwb_conversion_tools as ct
-import spikeextractors as se
-
 from tank_lab_to_nwb import TowersProcessedNWBConverter
 
 # Point to the base folder path for both recording data and Virmen
 base_path = Path("D:/Neuropixels/")
 
 # Name the NWBFile and point to the desired save path
-nwbfile_path = base_path / "TankProcessing_stub.nwb"
+nwbfile_path = base_path / "TowersTask.nwb"
 
 # Point to the various files for the conversion
 recording_folder = base_path / "Neuropixels" / "A256_bank1_2020_09_30" / "A256_bank1_2020_09_30_g0"
 spikeinterface_folder = recording_folder / "spikeinterface"
 raw_data_file = recording_folder / "A256_bank1_2020_09_30_g0_t0.imec0.ap.bin"
+raw_data_lfp_file = recording_folder / "A256_bank1_2020_09_30_g0_t0.imec0.lf.bin"
 recording_pickle_file = spikeinterface_folder / "raw.pkl"
 sorting_pickle_file = spikeinterface_folder / "sorter1.pkl"
 virmen_file_path = base_path / "TowersTask" / "PoissonBlocksReboot_cohort1_VRTrain6_E75_T_20181105.mat"
 
-# Enter Session and Subject information here
-# Comment out or remove any fields you do not want to include
+# Enter Session and Subject information here - uncomment any fields you want to include
 session_description = "Enter session description here."
 
 subject_info = dict(
     description="Enter optional subject description here",
-    weight="Enter subject weight here",
-    age=duration_isoformat(timedelta(days=0)),  # Enter the age of the subject in days
-    species="Mus musculus",
-    genotype="Enter subject genotype here",
-    sex="Enter subject sex here"
+    # weight="Enter subject weight here",
+    # age=duration_isoformat(timedelta(days=0)),  # Enter the age of the subject in days
+    # species="Mus musculus",
+    # genotype="Enter subject genotype here",
+    # sex="Enter subject sex here"
 )
 
+# Set some global conversion options here
+stub_test = True
+overwrite = True  # False if appending a file with sorting data - True if writing a brand new file with Virmen/SpikeGLX
 
-# Set some conversion options
-# Recommend setting stub_test to True for first conversion to allow fast testing/validation
-conversion_options = dict(
-    SIRecording=dict(stub_test=True),
-    SISorting=dict(stub_test=True)
-)
+
 
 # Run the conversion
 source_data = dict(
-    SIRecording=dict(pkl_file=str(recording_pickle_file.absolute())),
-    SISorting=dict(pkl_file=str(sorting_pickle_file.absolute())),
-    VirmenData=dict(file_path=str(virmen_file_path.absolute()))
+    SpikeGLXRecording=dict(file_path=str(raw_data_file)),
+    SpikeGLXLFP=dict(file_path=str(raw_data_lfp_file)),
+    VirmenData=dict(file_path=str(virmen_file_path))
+)
+conversion_options = dict(
+    SpikeGLXRecording=dict(stub_test=stub_test),
+    SpikeGLXLFP=dict(stub_test=stub_test)
 )
 converter = TowersProcessedNWBConverter(source_data)
 metadata = converter.get_metadata()
 metadata['NWBFile'].update(session_description=session_description)
 metadata['Subject'].update(subject_info)
-
-# TODO: Move to ProcessedNWBConverter, instantiate 'dummy' SpikeGLXRecordingInterface to use its get_metadata
-metadata.update(
-    ct.SpikeGLXRecordingInterface.get_ecephys_metadata(
-        spikeglx_meta=se.extractors.spikeglxrecordingextractor.readSGLX.readMeta(raw_data_file),
-        channel_ids=converter.data_interface_objects['SIRecording'].recording_extractor.get_channel_ids()
-    )
-)
-#####
-
 converter.run_conversion(
-    nwbfile_path=str(nwbfile_path.absolute()),
+    nwbfile_path=str(nwbfile_path),
     metadata=metadata,
-    conversion_options=conversion_options
+    conversion_options=conversion_options,
+    overwrite=overwrite
 )
